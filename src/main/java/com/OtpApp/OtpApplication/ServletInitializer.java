915a530b69606f620d46102fdc1fc64f125aa784
Arpit Srivastava <career.arpitsri@gmail.com>
2024-09-24 00:38:48 +0530

    initial commit

---
+++ src/main/java/com/OtpApp/OtpApplication/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.OtpApp.OtpApplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OtpApplication.class);
+	}
+
+}
